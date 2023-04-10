a359568d4b51006c6945a13de73483780fafa29c
shastye <clubbsm.53787@gmail.com>
2023-04-10 16:09:34 -0400

    Moved visibility of xml elements into their own methods. Screen1 = input of wifi information; Screen2 = visible qrcode and save button

--- app/src/main/java/com/example/qrinternet/Activities/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/qrinternet/Activities/dashboard/DashboardFragment.java
@@ -28,6 +28,20 @@ public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
+    ImageView qrCode;
+    Button createQRbutton;
+    Button saveQRbutton;
+    TextView ssid_tv;
+    EditText ssid_et;
+    TextView pw_tv;
+    EditText pw_et;
+    TextView sec_tv;
+    Spinner sec_s;
+    TextView hid_tv;
+    CheckBox hid_cb;
+    TextView fn_tv;
+    EditText fn_et;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel =
@@ -43,34 +57,25 @@ public class DashboardFragment extends Fragment {
         // ADDITIONS ADDED BETWEEN COMMENTS
 
 
-        ImageView qrCode = (ImageView) root.findViewById(R.id.ViewQRCode_imageView);
-        Button createQRbutton = (Button) root.findViewById(R.id.CreateQRCode_button);
-        Button saveQRbutton = (Button) root.findViewById(R.id.SaveQRCode_button);
+        //////////////////////////
+        //  Generating QR Code  //
+        //////////////////////////
 
-        TextView ssid_tv = (TextView) root.findViewById(R.id.ssid_textView);
-        EditText ssid_et = (EditText) root.findViewById(R.id.ssid_editText);
-        TextView pw_tv = (TextView) root.findViewById(R.id.password_textView);
-        EditText pw_et = (EditText) root.findViewById(R.id.password_editText);
-        TextView sec_tv = (TextView) root.findViewById(R.id.security_textView);
-        Spinner sec_s = (Spinner) root.findViewById(R.id.security_spinner);
-        TextView hid_tv = (TextView) root.findViewById(R.id.hidden_textView);
-        CheckBox hid_cb = (CheckBox) root.findViewById(R.id.hidden_checkBox);
-        TextView fn_tv = (TextView) root.findViewById(R.id.filename_textView);
-        EditText fn_et = (EditText) root.findViewById(R.id.filename_editText);
+        qrCode = (ImageView) root.findViewById(R.id.ViewQRCode_imageView);
+        ssid_tv = (TextView) root.findViewById(R.id.ssid_textView);
+        ssid_et = (EditText) root.findViewById(R.id.ssid_editText);
+        pw_tv = (TextView) root.findViewById(R.id.password_textView);
+        pw_et = (EditText) root.findViewById(R.id.password_editText);
+        sec_tv = (TextView) root.findViewById(R.id.security_textView);
+        sec_s = (Spinner) root.findViewById(R.id.security_spinner);
+        hid_tv = (TextView) root.findViewById(R.id.hidden_textView);
+        hid_cb = (CheckBox) root.findViewById(R.id.hidden_checkBox);
+        fn_tv = (TextView) root.findViewById(R.id.filename_textView);
+        fn_et = (EditText) root.findViewById(R.id.filename_editText);
+        createQRbutton = (Button) root.findViewById(R.id.CreateQRCode_button);
+        saveQRbutton = (Button) root.findViewById(R.id.SaveQRCode_button);
 
-        qrCode.setVisibility(View.INVISIBLE);
-        ssid_tv.setVisibility(View.VISIBLE);
-        ssid_et.setVisibility(View.VISIBLE);
-        pw_tv.setVisibility(View.VISIBLE);
-        pw_et.setVisibility(View.VISIBLE);
-        sec_tv.setVisibility(View.VISIBLE);
-        sec_s.setVisibility(View.VISIBLE);
-        hid_tv.setVisibility(View.VISIBLE);
-        hid_cb.setVisibility(View.VISIBLE);
-        fn_tv.setVisibility(View.INVISIBLE);
-        fn_et.setVisibility(View.INVISIBLE);
-        createQRbutton.setVisibility(View.VISIBLE);
-        saveQRbutton.setVisibility(View.INVISIBLE);
+        showScreen1();
 
         createQRbutton.setOnClickListener(new View.OnClickListener()
         {
@@ -105,19 +110,7 @@ public class DashboardFragment extends Fragment {
                         Bitmap bitmap = temp.getBitmap();
                         qrCode.setImageBitmap(bitmap);
 
-                        qrCode.setVisibility(View.VISIBLE);
-                        ssid_tv.setVisibility(View.INVISIBLE);
-                        ssid_et.setVisibility(View.INVISIBLE);
-                        pw_tv.setVisibility(View.INVISIBLE);
-                        pw_et.setVisibility(View.INVISIBLE);
-                        sec_tv.setVisibility(View.INVISIBLE);
-                        sec_s.setVisibility(View.INVISIBLE);
-                        hid_tv.setVisibility(View.INVISIBLE);
-                        hid_cb.setVisibility(View.INVISIBLE);
-                        fn_tv.setVisibility(View.VISIBLE);
-                        fn_et.setVisibility(View.VISIBLE);
-                        createQRbutton.setVisibility(View.INVISIBLE);
-                        saveQRbutton.setVisibility(View.VISIBLE);
+                        showScreen2();
                     }
                     else {
                         DialogFragment errorDialog = new ErrorCodeDialogFragment(temp.getResponseCode(), temp.getResponseDetails());
@@ -128,6 +121,12 @@ public class DashboardFragment extends Fragment {
             }
         });
 
+
+        //////////////////////
+        //  Saving QR Code  //
+        //////////////////////
+
+
         saveQRbutton.setOnClickListener(new View.OnClickListener()
         {
               @Override
@@ -135,7 +134,6 @@ public class DashboardFragment extends Fragment {
 
               }
         });
-
 
         // END ADDITIONS
 
@@ -146,5 +144,36 @@ public class DashboardFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void showScreen1() {
+        qrCode.setVisibility(View.INVISIBLE);
+        ssid_tv.setVisibility(View.VISIBLE);
+        ssid_et.setVisibility(View.VISIBLE);
+        pw_tv.setVisibility(View.VISIBLE);
+        pw_et.setVisibility(View.VISIBLE);
+        sec_tv.setVisibility(View.VISIBLE);
+        sec_s.setVisibility(View.VISIBLE);
+        hid_tv.setVisibility(View.VISIBLE);
+        hid_cb.setVisibility(View.VISIBLE);
+        fn_tv.setVisibility(View.INVISIBLE);
+        fn_et.setVisibility(View.INVISIBLE);
+        createQRbutton.setVisibility(View.VISIBLE);
+        saveQRbutton.setVisibility(View.INVISIBLE);
+    }
+    private void showScreen2() {
+        qrCode.setVisibility(View.VISIBLE);
+        ssid_tv.setVisibility(View.INVISIBLE);
+        ssid_et.setVisibility(View.INVISIBLE);
+        pw_tv.setVisibility(View.INVISIBLE);
+        pw_et.setVisibility(View.INVISIBLE);
+        sec_tv.setVisibility(View.INVISIBLE);
+        sec_s.setVisibility(View.INVISIBLE);
+        hid_tv.setVisibility(View.INVISIBLE);
+        hid_cb.setVisibility(View.INVISIBLE);
+        fn_tv.setVisibility(View.VISIBLE);
+        fn_et.setVisibility(View.VISIBLE);
+        createQRbutton.setVisibility(View.INVISIBLE);
+        saveQRbutton.setVisibility(View.VISIBLE);
     }
 }
