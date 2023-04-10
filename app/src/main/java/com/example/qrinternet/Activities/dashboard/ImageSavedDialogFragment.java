7a0f79762f82116bd4442a3bd5d9630e347f83e2
shastye <clubbsm.53787@gmail.com>
2023-04-10 17:38:31 -0400

    Added dialog message for successful image upload

---
+++ app/src/main/java/com/example/qrinternet/Activities/dashboard/ImageSavedDialogFragment.java
@@ -0,0 +1,27 @@
+package com.example.qrinternet.Activities.dashboard;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class ImageSavedDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {}
+        });
+        builder.setMessage("Image saved successfully.");
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
+
